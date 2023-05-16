5ef681c5507ec3693d027ec11c8a5502db42046a
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-05-16 09:41:17 +0300

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 	/* printing output using printf function*/
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
