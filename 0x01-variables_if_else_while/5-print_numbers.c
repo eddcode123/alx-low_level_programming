8a1df50131a725d7fbade3852ecce8f1984e28b6
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-05-16 12:48:09 +0300

    addingfiles

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	/* initialize varible a*/
+	int a = 0;
+
+	/* print base 10 numbers */
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	/* print newline */
+	printf("\n");
+	return (0);
+}
