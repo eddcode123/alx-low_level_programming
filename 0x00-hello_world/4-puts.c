1cc1b16aa46e59b1aeba054395edafb9440fa6d5
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-05-16 09:25:24 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 
-void main(void)
+int main(void)
 {
 	/*print a message to user using puts function*/
 	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
