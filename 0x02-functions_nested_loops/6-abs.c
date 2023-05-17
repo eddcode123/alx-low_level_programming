6aca66211385cd396d8d6a1ccc119335e3bf30c1
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-05-17 16:06:32 +0300

    adding 0x03-git

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ * @a: integer to convert to its absolute value
+ *
+ * Return: Absolute int value
+ */
+int _abs(int a)
+{
+	int b;
+
+	/*conver value to absolute */
+	b = abs(a);
+	return (b);
+}
