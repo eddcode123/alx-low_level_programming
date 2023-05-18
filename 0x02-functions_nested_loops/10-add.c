7238f3c92d2d5b51df6b00e136bd701a4092dce7
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-05-18 12:21:44 +0300

    adding 0x03-git

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @x: number to be added
+ * @y: second number to be add
+ * Return: sum of x and y
+ */
+int add(int x, int y)
+{
+	/* declare a variable sum to fimd sum of x and y */
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
