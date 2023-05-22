718b23aaf34e5dd8a0de4343594190ec1b54a37f
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-05-22 10:45:28 +0300

    addingfiles

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: number to be multiplied
+ * @b: number to be multiplied
+ *
+ * Return: result of a * b
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
