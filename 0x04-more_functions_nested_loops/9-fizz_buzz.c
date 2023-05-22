b8fae73e1dfcd043d135c2cd8954d755392ccca7
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-05-22 15:54:16 +0300

    addingfiles

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - replaces multiples of 3 & 5 with certain words
+ *
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	/* declare variables */
+	int i;
+	int n = 100;
+
+	/* use for loop to print numbers */
+	for (i = 1; i <= n; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
