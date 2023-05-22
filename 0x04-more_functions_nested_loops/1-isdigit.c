5eeb099a3ecca7853e43d7cfb28c3072f9ec782f
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-05-22 10:35:51 +0300

    addingfiles

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for base 10 numbers
+ * @c: number to be checked
+ *
+ * Return: 1 if 0-9 and 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return(0);
+}
