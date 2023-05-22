28d24fdc9b57bc1d7b6ac12e8c5cabab58f4fcc7
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-05-22 09:57:14 +0300

    addingfiles

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
- *
- * Return: always 0
+ * main - finds and prints the sum of the even-valued terms
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	/* initialize 1st and 2nd coz we have 1st 2 numbers */
-	int count;
-	long int first = 1, second = 2;
-	long int result, sum = 0;
-	/* use a loop to iterate upto 50 */
-	for (count = 3; count < 50; count++)
+	int i;
+	unsigned long int j, k, next, sum;
+
+	j = 1;
+	k = 2;
+	sum = 0;
+
+	for (i = 1; i <= 33; ++i)
 	{
-		/* initialize result */
-		result = first + second;
-		/* check for even numbers */
-		if (result % 2 == 0 && result <= 4000000)
-			sum += result;
-		/* asign 1st with 2nd and 2nd with result */
-		first = second;
-		second = result;
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		next = j + k;
+		j = k;
+		k = next;
 	}
-		printf("%lu\n", sum);
+
+	printf("%lu\n", sum);
+
 	return (0);
 }
