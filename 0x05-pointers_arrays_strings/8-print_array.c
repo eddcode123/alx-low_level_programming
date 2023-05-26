7a2dae808f327f0b62074d7883369c776548fbfe
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-05-26 12:58:33 +0300

    adding 0x03-git

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array -  prints n elements of an array of integers
+ * @n: print n elements
+ *
+ * Return: nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	/* print elements of array */
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n");
+}
