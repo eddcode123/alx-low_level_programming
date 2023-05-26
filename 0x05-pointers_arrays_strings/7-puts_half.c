09a880f31fbb814505b5332eed47cc920f9e2da4
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-05-26 12:25:34 +0300

    adding 0x03-git

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - print the second half of the string
+ * @str: string to print
+ *
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int i, middle, len;
+
+	/* find len of a string */
+	len = strlen(str);
+	/* find middle */
+	middle = len / 2;
+	/* use loop to print */
+	for (i = middle; i < len - 1; i++)
+	{
+		if (middle % 2 != 0)
+		{
+			for (i = middle; i < len; i++)
+				printf("%c", str[i]);
+		}
+		else
+			printf("%c", str[i]);
+	}
+	printf("\n");
+}
