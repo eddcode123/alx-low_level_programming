c9f63f7d63def53ae08104f77f03cd70dfeb407d
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-06-13 13:05:52 +0300

    addingfiles

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from memory area src to dest
+ * @n: bytes to be copied to dest
+ * @src: pointer to source string
+ * @dest: pointer to destination string
+ *
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *p = dest;
+	unsigned int i;
+
+	/* use for loop to copy n bytes to dest */
+	for (i = 0; i < n; i++)
+	{
+		*p = src[i];
+		p++;
+	}
+	*p = '\0';
+
+	return (dest);
+}
