e606577f481473f88c4f5db9e07b5219a195afa2
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-06-13 13:28:07 +0300

    addingfiles

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -16,8 +16,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	/* use for loop to copy n bytes to dest */
 	for (i = 0; i < n; i++)
 	{
-		*dest++ = src[i];
+		dest[i] = src[i];
 	}
-
 	return (dest);
 }
