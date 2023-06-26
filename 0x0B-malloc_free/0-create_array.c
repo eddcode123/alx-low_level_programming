0d9b5ee125b3d09a883017905e969df5f5e984a4
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-06-26 14:06:12 +0300

    addingfiles

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,22 +15,23 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	/* assign bytes to array using malloc */
-	ptr = (char *) malloc(size * sizeof(char));
 	/*check if size is 0 */
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	else
+	/* assign bytes to array using malloc */
+	ptr = (char *) malloc(size * sizeof(char));
+	/* if malloc has returned null */
+	if (ptr == NULL)
+		return (NULL);
+	/*initialize array with c*/
+	for (i = 0; i < size; i++)
 	{
-		/*initialize array with c*/
-		for (i = 0; i < size; i++)
-		{
-			ptr[i] = c;
-		}
-		return (ptr);
+		ptr[i] = c;
 	}
+	return (ptr);
 	free(ptr);
 
+	return (ptr);
 }
