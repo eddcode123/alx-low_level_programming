5207806a8bf9daff76475da14217473d6f554d6b
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-06-26 16:18:33 +0300

    addingfiles

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * _strdup - eturns a pointer to a new string which is a duplicate of the string str.
+ * _strdup - returns a pointer to a new string which is a duplicate of the string str.
  * @str: pointer to source string
  *
  * Return: pointer to duplicate malloc and null if malloc return null
@@ -11,27 +11,22 @@
 
 char *_strdup(char *str)
 {
-	char *ptr;
-	unsigned int i = 0, j = 0;
+	char *strout;
+	unsigned int i, j;
 
-	/* chech if str is null */
 	if (str == NULL)
 		return (NULL);
-	while (*str != '\0')
-	{
-		i += 1;
-		str++;
-	}
-	/* check if malloc return value is null */
-	ptr = (char *) malloc(i * sizeof(char) + 1);
-	if (ptr == NULL)
-	{
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	strout = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (strout == NULL)
 		return (NULL);
-	}
-	/* copy str to ptr */
+
 	for (j = 0; j <= i; j++)
-	{
-		ptr[j] = str[j];
-	}
-	return (ptr);
+		strout[j] = str[j];
+
+	return (strout);
 }
