83d63278478219d7fde16f0b505e520bd73355dd
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-06-26 16:25:45 +0300

    addingfiles

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,22 +11,24 @@
 
 char *_strdup(char *str)
 {
-	char *strout;
-	unsigned int i, j;
+	char *ptr;
+	unsigned int i = 0, j;
 
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; str[i] != '\0'; i++)
-		;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
 
-	strout = (char *)malloc(sizeof(char) * (i + 1));
+	ptr = (char *)malloc(sizeof(char) * (i + 1));
 
-	if (strout == NULL)
+	if (ptr == NULL)
 		return (NULL);
 
 	for (j = 0; j <= i; j++)
-		strout[j] = str[j];
+		ptr[j] = str[j];
 
-	return (strout);
+	return (ptr);
 }
