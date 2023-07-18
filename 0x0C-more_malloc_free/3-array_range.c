46cc82a343a95ab44eb61c9bade163c4c9e9d8d0
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-07-18 12:15:34 +0300

    addingfiles

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,21 +13,24 @@ int *array_range(int min, int max)
 {
 	/* create pointer to array */
 	int *a;
-	int i;
+	int i, size;
 	/* check if min is > max */
 	if (min > max)
 		return (NULL);
+	/* find size of array */
+	size = max - min;
 	/* allocate memory for array with malloc */
-	a = malloc(max * sizeof(int));
+	a = malloc(size * sizeof(int));
 
 	/* chech if malloc failed */
 	if (a == NULL)
 		return (NULL);
 
 	/* create an array of size max */
-	for (i = 0; i < max; i++, min++)
+	for (i = 0; i < size; i++, min++)
 	{
-		a[i] = min;
+		if (min < max)
+			a[i] = min;
 	}
 
 	return (a);
