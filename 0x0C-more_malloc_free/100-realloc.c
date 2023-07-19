e27fb07de0eaf2d1dc6b51f4191b5851023b7b96
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-07-19 11:52:16 +0300

    addingfiles

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,20 +13,19 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	/* test if size of old is == to new */
-	if (old_size == new_size)
-		return (ptr);
-	/* check if ptr is null */
-	if (ptr == NULL)
-	{
-		/* allocate new memory */
-		ptr = malloc(new_size);
-	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+
+	if (new_size == old_size)
+		return (ptr);
+
+
 	free(ptr);
 	ptr = malloc(new_size);
 
