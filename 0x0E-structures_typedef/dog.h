054bcc4ad06decb102c07155400975cd1619fd19
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-07-20 10:20:09 +0300

    addingfiles

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/**
+ * struct dog - data staructer containing dog info
+ * @name: pointer to name of dog
+ * @age: age of dog
+ * @owner: name of dog owner
+ *
+ * description: basically this data staructer will be used to 
+ * to collect data on dog name age and dog owner name
+ *
+ */
+struct gog
+{
+	char *name;
+	float age;
+	char *owner;
+};
