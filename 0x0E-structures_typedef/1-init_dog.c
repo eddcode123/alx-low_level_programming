6de96f13661849df8bbb8718adcbc8cb41f410e9
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-07-20 11:25:57 +0300

    addingfiles

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,8 +12,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	/* initializing d with using struct dog */
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	if (d)
+	{
+		/* initializing d with using struct dog */
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
 }
