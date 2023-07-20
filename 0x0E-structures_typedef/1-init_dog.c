9a5f2c125bf8eb0cec987adece61653a5c72acd2
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-07-20 11:22:55 +0300

    addingfiles

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,13 +2,14 @@
 
 /**
  * init_dog - initializes a variable of type struct dog
- * @d: pointer to initialized variable
+ * @d: pointer to initialized struct dog
  * @name: member one
  * @age: member two
  * @owner: member three
  *
  * Return: nothing
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	/* initializing d with using struct dog */
