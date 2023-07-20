41d581a9c17f9467f5c6a3f42488a26df23b8064
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-07-20 11:51:48 +0300

    addingfiles

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,17 +10,18 @@
 
 void print_dog(struct dog *d)
 {
-	/* check if d is null */
 	if (d)
 	{
-		if (d == NULL)
-			return;
-		if ((*d).name == NULL)
-			printf("Name: (nill)\n");
-		if ((*d).age == 0)
-			printf("Age: (nill)\n");
-		if ((*d).owner == NULL)
-			printf("Owner: (nill)\n");
-		printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
+		if (!(*d).name)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", (*d).name);
+
+		printf("Age: %f\n", (*d).age);
+
+		if (!(*d).owner)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", (*d).owner);
 	}
 }
