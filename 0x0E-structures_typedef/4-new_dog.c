9426a55731c5ab913e7e3e954fbce83b4459fb17
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-07-24 10:15:02 +0300

    addingfiles

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - create a new dog
@@ -14,18 +15,45 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	/* create a new dog pointer */
 	dog_t *newdog;
+	/* declare variables to use */
+	int lname, lowner, i;
+
 
 	/* use malloc to allocate memory to new dog */
-	newdog = malloc(sizeof(dog_t));
+	newdog = malloc(sizeof(*newdog));
 
 	/* check if malloc failed */
-	if (newdog == NULL)
+	if (newdog == NULL || !(name) || !(owner))
+	{
+		free(newdog);
 		return (NULL);
+	}
+	/* find len of owner and name */
+	lname = strlen(name);
+	lowner = strlen(owner);
+	/* allocate name of newdog size of name */
+	newdog->name = malloc(sizeof(lname + 1));
+	/* allocate owmer of newdog size of owner */
+	newdog->owner = malloc(sizeof(lowner + 1));
 
+	/* check for failure */
+	if (!(newdog->name) || !(newdog->owner))
+	{
+		free((newdog->name));
+		free((newdog->owner));
+		free(newdog);
+		return (NULL);
+	}
 	/* assign members respectively */
-	newdog->name = name;
+	for (i = 0; i < lname; i++)
+		newdog->name[i] = name[i];
+	newdog->name[i] = '\0';
+
 	newdog->age = age;
-	newdog->owner = owner;
+
+	for (i = 0; i < lowner; i++)
+		newdog->owner[i] = owner[i];
+	newdog->owner[i] = '\0';
 
 	return (newdog);
 }
