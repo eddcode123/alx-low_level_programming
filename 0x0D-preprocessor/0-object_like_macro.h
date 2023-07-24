6aa0077af293efdb70950e611eea51812a00d714
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-07-24 12:43:02 +0300

    addingfiles

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,10 +1,1 @@
-#ifndef _0-OBJECT_LIKE_MACRO_H_
-#define _0-OBJECT_LIKE_MACRO_H_
-
-/**
- * SIZE - macro abriviation for toke 1024.
- */
-
 #define SIZE 1024
-
-#endif /* _0-OBJECT_LIKE_MACRO_H_ */
