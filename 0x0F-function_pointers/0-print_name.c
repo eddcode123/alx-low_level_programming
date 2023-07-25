ca6454a38cd69a92d6fa56ac5e5031f8472c4aff
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-07-25 10:25:52 +0300

    addingfiles

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,15 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: name to be printend
+ * print_name - prints a name.
+ * @name: input name.
  * @f: function pointer
  *
  * Return: nothing
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	/* call function passed and paremeters using pointer */
+	/* call function using pointer */
 	if (name && f)
 		f(name);
 }
