5a5b7a99c9674a9cf2e84c91eede0a6e3f1e88e1
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-07-27 12:43:11 +0300

    addingfiles

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,14 +13,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	/* initialize list */
 	va_list(args);
 	unsigned int i;
-	/* check if sperator is null */
-	if (separator == NULL)
-		return;
 	/* call va_start */
 	va_start(args, n);
 	/* use for loop to print args passed */
 	for (i = 0; i < n; i++)
 	{
+		if (separator == NULL)
+			printf("%d\n", va_arg(args, int));
 		if (i != (n - 1))
 			printf("%d%s", va_arg(args, int), separator);
 		else
