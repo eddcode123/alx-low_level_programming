68e145b655d1295d04411c12890af7d34fc3ccda
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-02 15:59:54 +0300

    addingfiles

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @head: head of list
+ *
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	/* declare current pointer */
+	list_t *current;
+
+	/* use a while loop to free node */
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current ->str);
+		free(current);
+	}
+}
