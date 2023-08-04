83c33876015c97d3f595ca58c6ed2b2878890e2e
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-04 13:01:32 +0300

    addingfiles

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list.
+ * @head: head of list
+ *
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	/* declare current pointer */
+	listint_t *current;
+	listint_t *temp;
+	/* point temp to where head is pointing */
+	temp = *head;
+	/* check if list empty */
+	if (temp == NULL)
+		return;
+	/* traverse the list and free each node */
+	while ((current = temp) != NULL)
+	{
+		temp = temp->next;
+		free(current);
+	}
+	/* set head to NULL */
+	*head = NULL;
+}
