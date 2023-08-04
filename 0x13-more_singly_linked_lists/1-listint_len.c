cd72b9bd37a0e71de48b4481db8b93bc61f02e93
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-04 12:00:07 +0300

    addingfiles

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints the elements of listint_t list.
+ * @h: head of the list
+ *
+ * Return: number of node
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	/* declare a count variable */
+	size_t count = 0;
+
+	/* traverse the list while h is not null*/
+	while (h != NULL)
+	{
+		h = h->next;
+		/* use count to find len of list */
+		count++;
+	}
+	return (count);
+}
