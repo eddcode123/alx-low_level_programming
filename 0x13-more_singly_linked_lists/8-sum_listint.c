7481dc652175641d65eb37f0a2722e16833e96a0
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-04 16:31:35 +0300

    addingfiles

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * sum_listint - finds sum of n in the list
+ * @head: head of the list.
+ *
+ * Return: sum of all the data (n) of a listint_t linked list.
+ * or 0 if list is empty
+ */
+
+int sum_listint(listint_t *head)
+{
+	/* declare a variable sum */
+	int sum = 0;
+
+	/* check if the list is empty */
+	if (head == NULL)
+		return (0);
+	/* add all the n data to sum in the list */
+	while (head->next != NULL)
+	{
+		head = head->next;
+		sum += head->n;
+	}
+
+	return (sum);
+}
