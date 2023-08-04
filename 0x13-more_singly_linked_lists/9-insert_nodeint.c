e2d29bbbe14bb71abed7be67fad4764fde327266
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-04 17:21:15 +0300

    addingfiles

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: head of the list
+ * @idx: index to insert newnode
+ * @n: value to add to newnode
+ *
+ * Return: address of newnode
+ * or NULL if failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	/* declare newnode */
+	listint_t *newnode;
+	listint_t *temp;
+	listint_t *nextnode;
+	/* declare a vaariable to get index */
+	size_t i = 0;
+
+	/* allocate memory to newnode */
+	newnode = (listint_t *) malloc(sizeof(listint_t));
+
+	/* check if malloc failed */
+	if (newnode == NULL)
+		return (NULL);
+	/* insert value to new node */
+	newnode->n = n;
+
+	/* point temp where head is pointing */
+	temp = *head;
+
+	/* check if list is empty */
+	if (temp == NULL)
+	{
+		/* assign head to newnode */
+		*head = newnode;
+		/* point newnode -> to null */
+		newnode->next = NULL;
+	}
+	/* if list is not empty add newnode at index */
+	while (temp->next != NULL && i < idx - 1)
+	{
+		temp = temp->next;
+		i++;
+	}
+	nextnode = temp->next;
+	newnode->next = nextnode;
+	temp->next = newnode;
+
+	return (newnode);
+}
