b7e8485085ccf6c42ab9171c53b65250379e5573
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-04 16:11:08 +0300

    addingfiles

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -29,7 +29,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 			count++;
 		}
 		/* check if index exist */
-		if (index >= count)
+		if (index - 1 > count)
 			return (NULL);
 		/* reset temp to the start of the list */
 		temp = head;
