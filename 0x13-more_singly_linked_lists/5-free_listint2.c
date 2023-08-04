bbc8d6a96233e655fb1763e0496d0a488b672f9d
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-04 13:12:50 +0300

    addingfiles

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,7 +14,7 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 
 	/* check if list is not empty */
-	if (*head != NULL)
+	if (head != NULL)
 	{
 		/* point temp to where head is pointing */
 		temp = *head;
