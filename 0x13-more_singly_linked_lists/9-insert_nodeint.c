43969aa14c278b47f21bbf8804c43adbb5d631fc
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-07 10:03:19 +0300

    addingfiles

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,8 +43,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (temp->next != NULL && i < idx - 1)
 	{
 		prevnode = temp;
-		if (prevnode->next == NULL)
-			return (NULL);
 		temp = temp->next;
 		i++;
 	}
