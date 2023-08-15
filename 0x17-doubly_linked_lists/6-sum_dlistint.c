911353a55e02df9055c368a03b0d882494cca5be
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-15 16:32:28 +0300

    bettyfix

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - finds sum of all the data in the list.
+ * @head: head of list.
+ *
+ * Return: sum of data
+ * or 0 if list is empty
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	/* declare sum varaible and initialize it to zero */
+	int sum = 0;
+
+	/*check if list is empty */
+	if (head == NULL)
+		return (0);
+	/* travers list and to acess data of each list */
+	while (head->next != NULL)
+	{
+		/* point head to next node */
+		head = head->next;
+		/* find sum of all data */
+		sum += head->n;
+	}
+
+	return (sum);
+}
