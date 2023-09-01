f41329da492294699c3b28a5768d88d597111072
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-09-01 14:10:32 +0300

    addingfiles

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,30 +11,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int len1, len2, i;
+	int result = strcmp(s1, s2);
 
-	/* check if strings are empty */
-	if (s1 == NULL || s2 == NULL)
-		return (-1);
-
-	/* find len of strings */
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-
-	/* compare lens */
-	if (len1 > len2)
-		return (len1 - len2);
-	else if (len1 < len2)
-		return (len1 - len2);
-
-	/* loop through strings and compare charactres */
-	for (i = 0; i < len1; i++)
-	{
-		if (s1[i] < s2[i])
-			return (s1[i] - s2[i]);
-		else if (s1[i] > s2[i])
-			return (1);
-	}
-
-	return (0);
+	return (result);
 }
