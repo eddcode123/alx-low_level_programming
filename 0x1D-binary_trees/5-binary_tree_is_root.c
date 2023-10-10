f2d20b59545c83f503795bde276ebbb024f20a29
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-10-10 14:34:49 +0300

    addingfiles

---
+++ 0x1D-binary_trees/5-binary_tree_is_root.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if a node is a root node
+ * @node: node to be checked
+ *
+ * Return: returns 1 if root node 0 otherwise
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	/* initialize "root" to 1 if node is root node 0 otherwise */
+	int root = 0;
+
+	/* check if node is a root node */
+	if (node && (node->parent == NULl))
+	{
+		root = 1;
+	}
+
+	return (root);
+}
