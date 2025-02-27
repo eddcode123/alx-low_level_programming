43c233b321c140a171fbda96369cc5cb2a995e77
eddcode123 <edwinmutugi29@gmail.com>
2025-02-27 10:19:55 +0300

    function free a linked list passed by reference

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,29 +2,24 @@
 
 /**
  * free_listint2 - frees a listint_t list.
- * @head: head of list
+ * @head: pointer to the first node in the list
  *
  * Return: Nothing
  */
 
 void free_listint2(listint_t **head)
 {
-	/* declare current pointer */
-	listint_t *current;
+	/* temp var hold address of prev node */
 	listint_t *temp;
 
-	/* check if list is not empty */
-	if (head != NULL)
+	/* traverse list and free nodes */
+	while (*head != NULL)
 	{
-		/* point temp to where head is pointing */
 		temp = *head;
-		/* traverse the list and free each node */
-		while ((current = temp) != NULL)
-		{
-			temp = temp->next;
-			free(current);
-		}
-		/* set head to NULL */
-		*head = NULL;
+		*head = (*head)->next;
+
+		free(temp);
 	}
+
+	*head = NULL;
 }
