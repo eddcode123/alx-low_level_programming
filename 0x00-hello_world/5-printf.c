449c80f50246fab5ae094550e5776d7b3fd8bf8f
eddcode123 <edwinmutugi29@gmail.com>
2025-03-25 14:48:14 +0300

    Uses printf to output fixed string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints a string using printf
+ * Description: Uses printf to output fixed message
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
