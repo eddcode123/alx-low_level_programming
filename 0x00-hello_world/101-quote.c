cf86807d3a7172b09fbb41a41804dbf348c84077
eddcode123 <edwinmutugi29@gmail.com>
2025-03-25 16:03:53 +0300

    prints fixed message to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
-/**
- * main - prints a fixed message
- * Description: prints fixed message to the standard error
- * Return: 1
- */
 
+/**
+ * main - Prints a fixed message to the standard error
+ *
+ * Description: Outputs a specific string to standard error
+ * using the write system call.
+ *
+ * Return: Always 1 (Error status)
+ */
 int main(void)
 {
-	write(2, \
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
