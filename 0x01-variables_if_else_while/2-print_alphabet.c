c88acef14fd30936fb0ddf8a4590067885c5f9d7
eddcode123 <edwinmutugi29@gmail.com>
2025-03-26 14:10:59 +0300

    prints the alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
- /**
-  * main - prints the alphabet in lowercase
-  * Return: Always (0)
+/**
+ * main - Prints the entire alphabet in lowercase letters
+ * Return: Always (0)
 */
-
 int main(void)
 {
 	int alpha;
+
 	for (alpha = 97; alpha < 123; alpha++)
 	{
 		putchar(alpha);
