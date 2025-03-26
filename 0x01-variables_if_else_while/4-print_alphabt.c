42b51ade765e3233c532c8f94de38b72285b0409
eddcode123 <edwinmutugi29@gmail.com>
2025-03-26 14:45:05 +0300

    Prints entire alphabet excluding the characters: 'q' and 'e'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet excluding q and e
+ * Return: Always (0)
+*/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' || ch != 'e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
