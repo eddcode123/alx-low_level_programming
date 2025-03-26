cdb4eb90187e7a84b711cd2386be08d469a96bf6
eddcode123 <edwinmutugi29@gmail.com>
2025-03-26 15:21:48 +0300

    prints base 16 numbers in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints base 16 numnbers in lowercase
+ * Return: Always (0)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 17; i++)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0');
+		}
+		else
+		{
+			putchar(i % 10 + 'a');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
