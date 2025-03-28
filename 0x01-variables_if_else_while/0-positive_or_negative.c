6f9bd106fc5abf58628071b7f84d5aab2649d8af
eddcode123 <edwinmutugi29@gmail.com>
2025-03-28 12:47:43 +0300

    Program check is a number is positive, negative or zero and prints the number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,33 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 /**
- * main - Generates a random number and prints whether
- *        it is positive, negative, or zero.
- * Return: 0
+ * main - checks whether a number is positve or negative
+ * Return: Always(0)
 */
 
+/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
+
+	printf("%d", n);
+
+	if (n == 0)
 	{
-		printf("%d is negative\n", n);
+		printf(" is zero\n");
 	}
 	else if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf(" is positive\n");
 	}
 	else
 	{
-		printf("%d is zero\n", n);
+		printf(" is negative\n");
 	}
-
+	
 	return (0);
 }
