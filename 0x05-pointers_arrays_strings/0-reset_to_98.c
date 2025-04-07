6f081c20fddb10ff3fbc7025b26dc650b2e637b7
eddcode123 <edwinmutugi29@gmail.com>
2025-04-07 13:05:00 +0300

    Reassign value pointed to by n

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value it points to to 98.
- * @n: pointer to reset value
- *
- * Return: nothing
- */
+ * reset_to_98 - Reassignes the value of a pointer
+ * to 98
+ * @n: pointer to the value stored
+ * 
+ * Return: Nothing
+*/
 
 void reset_to_98(int *n)
 {
