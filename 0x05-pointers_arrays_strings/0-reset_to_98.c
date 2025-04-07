cd5a21fd14cbbf0c18374773fffad71569832b06
eddcode123 <edwinmutugi29@gmail.com>
2025-04-07 13:07:48 +0300

    Reassigns the value of a pointer to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - Reassignes the value of a pointer
- * to 98
- * @n: pointer to the value stored
- * Return: Nothing
+* reset_to_98 - Reassigns the value of a pointer to 98
+* @n: pointer to the value stored
+* Return: Nothing
 */
 
 void reset_to_98(int *n)
