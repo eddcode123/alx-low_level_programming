c5558d615860199f264d87b76f9f3ab0d8f50e13
eddcode123 <edwinmutugi29@gmail.com>
2025-04-16 12:37:19 +0300

    C program that converts a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,40 +1,38 @@
-#include "main.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <ctype.h>
 
 /**
- * _atoi - Convert a string to an integer.
- * @s: The pointer to convert
+ * _atoi - Converts a string to an integer.
+ * @s: Pointer to the first character of the string.
  *
- * Return: A integer
+ * Return: The integer value of the string, or 0 if no valid integer is found.
  */
 int _atoi(char *s)
 {
-	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
+	int result = 0;
+	int sign = 1; /* Default to positive */
+	bool set = false;
 
-	while (s[c])
+	while (*s != '\0')
 	{
-		if (s[c] == 45)
+		/* Check for sign */
+		if (*s == '-' && !set)
 		{
-			min *= -1;
+			sign *= -1;
 		}
-
-		while (s[c] >= 48 && s[c] <= 57)
+		else if (isdigit(*s))
 		{
-			isi = 1;
-			ni = (ni * 10) + (s[c] - '0');
-			c++;
+			result = result * 10 + (*s - '0');
+			set = true; /* Start processing number */
 		}
-
-		if (isi == 1)
+		/* Break on invalid character after starting the number */
+		else if (set)
 		{
 			break;
 		}
-
-		c++;
+		s++;
 	}
 
-	ni *= min;
-	return (ni);
+	return (result * sign);
 }
