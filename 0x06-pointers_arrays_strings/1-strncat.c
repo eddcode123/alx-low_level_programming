c7c64170628bd2510dc508eba55dc5c840c1bc32
eddcode123 <edwinmutugi29@gmail.com>
2025-04-21 13:24:19 +0300

    Logic fix  - fixing a logical problem

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,32 +1,31 @@
 #include "main.h"
 
 /**
- * _strncat - Function that concatenates n bytes of two strings.
+ * _strncat - Function that concatenates up to n bytes of two strings.
  * @dest: Pointer to the first string (destination).
  * @src: Pointer to the second string (source).
- * @n: n of bytes of src to concatenate
+ * @n: Number of bytes of src to concatenate.
  * Return: Pointer to the concatenated string.
-*/
-
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	int cursor = 0;
-	char *str = src;
+	int cursor = 0, i = 0;
 
-	while (src[cursor] != '\0')
+	/* Move to the end of the dest string */
+	while (dest[cursor] != '\0')
 	{
-		if (src[cursor] < n)
-		{
-			cursor++;
-		}
-	}
-
-	while (*str != '\0')
-	{
-		dest[cursor] = *str;
-		str++;
 		cursor++;
 	}
+
+	/* Append at most n bytes of src to dest */
+	while (i < n && src[i] != '\0')
+	{
+		dest[cursor] = src[i];
+		cursor++;
+		i++;
+	}
+
+	/* Null-terminate the result */
 	dest[cursor] = '\0';
 
 	return (dest);
