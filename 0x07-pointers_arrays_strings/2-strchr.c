0e78438426674dcb5c1a6bac144ae16184136071
eddcode123 <edwinmutugi29@gmail.com>
2025-04-28 10:53:25 +0300

    C program that located a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strchr -  Function locates a character in a string
@@ -21,5 +20,5 @@ char *_strchr(char *s, char c)
 		temp++;
 	}
 
-	return (NULL);
+	return (0);
 }
