753d72f90926b6e4e30be3d534e95ba7c4c97fc0
eddcode123 <edwinmutugi29@gmail.com>
2025-04-28 09:36:00 +0300

    C program that sets a buffer with a specific value

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,19 @@
 #include "main.h"
 
 /**
- * _memset - fills the first n bytes of the memory area
- * @s: pointer to string
- * @b: constant byte to fill n bytes
- * @n: bytes to be filled
- *
- * Return: pointer to resulting string
- */
-
+ * _memset - Function that sets a buffer with a specific value
+ * @s: pointer to the starting address of buffer to be set.
+ * @b: data to fill the buffer
+ * @n: size of buffer
+ * Return: pointer to the set buffer
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *p = s;
-	unsigned int i;
+	int i;
 
 	for (i = 0; i < n; i++)
 	{
-		p[i] = b;
+		s[i] = b;
 	}
 
 	return (s);
