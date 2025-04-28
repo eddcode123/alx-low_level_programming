28e938c975c46fa8419662eb7dd025e44a582739
eddcode123 <edwinmutugi29@gmail.com>
2025-04-28 11:27:55 +0300

    C program that calulated the length of the initial string that consist of a set of another string

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,31 +1,34 @@
 #include "main.h"
 
 /**
- * _strspn - Function that calculates the len of the initial string
- * that consist of a set of string
- * @s: pointer to the original string
- * @accept: the set of strings to compare to s
- * Return: length of matched set or 0 if the first character of the
- * original string doesn't match
+ * _strspn - Function that calculates the length of the initial segment of `s`
+ * that consists only of characters in `accept`.
+ * @s: pointer to the original string.
+ * @accept: the set of characters to compare to `s`.
+ * Return: length of the matched segment.
 */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int count = 0;
 	int i, j;
+	int match_found;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		match_found = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] != accept[j])
+			if (s[i] == accept[j])
 			{
-				return (count);
-			}
-			else
-			{
-				count++;
+				match_found = 1;
+				break;
 			}
 		}
+		if (!match_found)
+		{
+			break;
+		}
+		count++;
 	}
 
 	return (count);
