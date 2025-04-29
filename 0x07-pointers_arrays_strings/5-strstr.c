90fc5da208f4a358bb1c70a136f5c5271562df69
eddcode123 <edwinmutugi29@gmail.com>
2025-04-29 10:23:34 +0300

    C program that check if a string is a substring of another string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strstr - Function that locates a substring in a string
+ * @haystack: string to be scaned for substring
+ * @needle: substring to be searched in haystack
+ * Return: pointer to the first occurance of needle in haystack
+ * else null
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	char *n, *h;
+
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+
+	while (haystack)
+	{
+		h = haystack;
+		n = needle;
+
+		while (h && n && (*n == *h))
+		{
+			n++;
+			h++;
+		}
+
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+
+	return (NULL);
+}
