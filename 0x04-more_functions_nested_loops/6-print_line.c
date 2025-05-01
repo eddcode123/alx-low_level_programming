cdbdb5bbd6cd6b2e3a8febec6f5b5f25a68cc05d
eddcode123 <edwinmutugi29@gmail.com>
2025-05-01 13:16:06 +0300

    C program that prints a line to the stdout

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,14 +9,12 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-	}
-
-	for (i = 0; i < n; i++)
-	{
-		_putchar('_');
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
 	}
 	_putchar('\n');
 }
