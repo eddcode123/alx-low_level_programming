a0a97a7f2931ec68084e4d5072ca2de53f0fe245
eddcode123 <edwinmutugi29@gmail.com>
2025-05-05 12:07:18 +0300

    C program that prints a square to the standard out

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,29 +1,24 @@
 #include "main.h"
 
 /**
- * print_square - prints a square, followed by a new line
- * @size: size of the square
- *
+ * print_square - Function that prints a square to the standard out
+ * @size: the size of the square
  * Return: nothing
- */
+*/
 void print_square(int size)
 {
-	/* declare two variables */
 	int i, j;
 
-	/* use if to check the condition for size */
 	if (size <= 0)
-		_putchar('\n');
-	else
 	{
-		/* use nested for loop to print square */
-		for (i = 0; i < size; i++)
+		_putchar('\n');
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
 		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
+			_putchar('#');
 		}
+		_putchar('\n');
 	}
 }
