e2decf7c6c6c4cd94b73aef0588254793e65fba0
eddcode123 <edwinmutugi29@gmail.com>
2025-05-12 19:42:41 +0300

    C program that uses recursion to print a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line.
- * @s: pointer to string to be printed
- *
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to print
  * Return: nothing
- */
-
+*/
 void _puts_recursion(char *s)
 {
 	/* base case */
@@ -15,9 +13,6 @@ void _puts_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
