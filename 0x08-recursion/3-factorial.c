1fa0f6d137b2bc60579b00efbcd160943ec5938d
eddcode123 <edwinmutugi29@gmail.com>
2025-05-13 15:13:51 +0300

    C program that computes the factorial of a given number as input

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,26 +1,23 @@
 #include "main.h"
 
 /**
- * factorial - returns the factorial of a given number.
- * @n: number to find factorial
- *
- * Return: returns the factorial of a given number & -1 for negative
- *
- */
+ * factorial - computes the factorial of a given number
+ * @n: number to be computed
+ * Return: factorial  of the given number or -1
+*/
 
 int factorial(int n)
 {
-	/* set base case */
 	if (n < 0)
 	{
 		return (-1);
 	}
-	if (n == 1)
+
+	/* base case */
+	if (n == 0)
 	{
 		return (1);
 	}
-	else
-	{
-		return (n * factorial(n - 1));
-	}
+
+	return (n * factorial(n - 1));
 }
