ac88ffe79b54c030abbebe6032692795dd85c8b3
eddcode123 <edwinmutugi29@gmail.com>
2025-05-13 15:01:36 +0300

    C program that computes the length of a string using recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string.
- * @s:pointer to dtring to find the length.
- *
- * Return: nothing
- */
-
+ * _strlen_recursion - Finds the len of a string
+ * @s: string in question
+ * Return: length of the string
+*/
 int _strlen_recursion(char *s)
 {
 	int len = 0, count = 1;
-	/* set base case */
+
+	/* base case */
 	if (*s == '\0')
 	{
 		return (0);
 	}
-	else
-	{
-		len = len + count + _strlen_recursion(s + 1);
-		return (len);
-	}
+
+	len = len  + count + _strlen_recursion(s + 1);
+	return (len);
 }
