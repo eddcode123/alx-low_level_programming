edb8cd65a608c82b49ef8b8ec1caedeaa4c5a73d
eddcode123 <edwinmutugi29@gmail.com>
2025-05-26 16:43:06 +0300

    C program that computes multiplication of 2 numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - function that computes the multiplication of two arguments
@@ -12,7 +13,7 @@ int main(int argc, char **argv)
 
 	if (argc == 3)
 	{
-		printf("%d\n", (int)argv[1] * (int)argv[2]);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
 	printf("Error\n");
