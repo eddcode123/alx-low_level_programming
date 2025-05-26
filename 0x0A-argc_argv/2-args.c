30245df421bedb04fb8d1d399ba9c38bf567d19c
eddcode123 <edwinmutugi29@gmail.com>
2025-05-26 16:22:29 +0300

    C program that prints the arguments passed to it

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - print all arguments passed
- * @argc: number of arguments passed
- * @argv: array of strings
- *
- * Return: 0 for success
- */
-
+ * main - function that prints the arguments passed
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+*/
 int main(int argc, char **argv)
 {
-	/* use for loop to print argumrents */
-	int i;
+	int count;
 
-	for (i = 0; i < argc; i++)
+	for (count = 0; count <= argc; count++)
 	{
-		printf("%s\n", argv[i]);
+		printf("%s\n", argv[count]);
 	}
+
 	return (0);
 }
