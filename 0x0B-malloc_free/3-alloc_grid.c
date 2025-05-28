ba1adf9d0a5135fc3f6c82861ef49c4edc8bb4e6
eddcode123 <edwinmutugi29@gmail.com>
2025-05-28 13:02:32 +0300

    C program that creates 2d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,43 +1,52 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * alloc_grid - returns a pointer to a 2 dimensional array of integers.
- * @width: width of the array.
- * @height: height of the array.
- *
- * Return: pointer of an array of integers
- */
+ * alloc_grid - creates a pointer to a 2d array
+ * @height: height of grid
+ * @width: width of grid
+ * Return: pointer to array of NULL
+*/
 int **alloc_grid(int width, int height)
 {
-	int **gridout;
 	int i, j;
+	int **array;
 
-	if (width < 1 || height < 1)
-		return (NULL);
-
-	gridout = malloc(height * sizeof(int *));
-	if (gridout == NULL)
+	if (width <= 0 || height <= 0)
 	{
-		free(gridout);
+		return (NULL);
+	}
+
+	/* allocate memory */
+	array = (int *) malloc(height * sizeof(int *));
+
+	/* check for malloc failure */
+	if (!array)
+	{
 		return (NULL);
 	}
 
 	for (i = 0; i < height; i++)
 	{
-		gridout[i] = malloc(width * sizeof(int));
-		if (gridout[i] == NULL)
+		array[i] = malloc(width * sizeof(int));
+		if (array[i] == NULL)
 		{
 			for (i--; i >= 0; i--)
-				free(gridout[i]);
-			free(gridout);
+			{
+				free(array[i]);
+			}
+			free(array);
 			return (NULL);
 		}
 	}
 
 	for (i = 0; i < height; i++)
+	{
 		for (j = 0; j < width; j++)
-			gridout[i][j] = 0;
+		{
+			array[i][j] = 0;
+		}
+	}
 
-	return (gridout);
+	return (array);
 }
