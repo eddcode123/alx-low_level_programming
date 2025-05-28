e06e2600535f7fe97eda8855020c8389c6a9bf5e
eddcode123 <edwinmutugi29@gmail.com>
2025-05-28 11:21:50 +0300

    C program that created and initializes an array using dynamic memory allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,6 +10,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array;
+	char *temp;
 	unsigned int i;
 
 	if (size <= 0)
@@ -24,7 +25,8 @@ char *create_array(unsigned int size, char c)
 	{
 		array[i] = c;
 	}
-	return (array);
-	free(array);
+	temp = array;
+
+	free(temp);
 	return (array);
 }
