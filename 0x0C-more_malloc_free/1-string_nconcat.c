a9bf3a7d0b1651e4d972af544ef3417bc7ce9cb9
eddcode123 <edwinmutugi29@gmail.com>
2025-06-03 13:21:05 +0300

    C program that concatinates 1st string with 2nd string n bytes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,57 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * string_nconcat - Function that concatinates 2 strings
- * @s1: pointer to the string
- * @s2: pointer to string
- * @n: number of n bytes to copy from s2
- * Return: pointer to concatinated string or NULL
-*/
+ * string_nconcat - Concatenates two strings.
+ * @s1: Pointer to the first string.
+ * @s2: Pointer to the second string.
+ * @n: Number of bytes to copy from s2.
+ *
+ * Return: Pointer to the concatenated string or NULL.
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int i, j, len_1, len_2;
+	unsigned int i, j;
+	unsigned int len_1, len_2;
 
-	/* handle edge cases */
+	/* Treat NULL inputs as empty strings */
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	if (n <= 0)
-	{
-		return (NULL);
-	}
-	/* find len of strings */
-	for (len_1 = i = 0; s1[i] != '\0'; i++)
-	{
-		len_1++;
-	}
-	for (len_2 = i = 0; s2[i] != '\0' && i < n; i++)
-	{
-		len_2++;
-	}
 
-	/* allocate memory */
-	str = (char *) malloc((len_1 + len_2 + 1) * sizeof(char));
+	/* Calculate the lengths of s1 and s2 */
+	for (len_1 = 0; s1[len_1] != '\0'; len_1++)
+		;
+	for (len_2 = 0; s2[len_2] != '\0'; len_2++)
+		;
 
-	if (!str)
-	{
+	/* Limit len_2 to n bytes if n is smaller */
+	if (len_2 > n)
+		len_2 = n;
+
+	/* Allocate memory for the new string */
+	str = malloc((len_1 + len_2 + 1) * sizeof(char));
+	if (str == NULL)
 		return (NULL);
-	}
-	for (i = 0; s1[i] != '\0'; i++)
-	{
+
+	/* Copy s1 into the new string */
+	for (i = 0; i < len_1; i++)
 		str[i] = s1[i];
-	}
+
+	/* Append the first len_2 characters of s2 to the new string */
 	for (j = 0; j < len_2; j++)
-	{
-		str[i + j] = s2[j];
-	}
-	str[i + j] = '\0';
+		str[len_1 + j] = s2[j];
+
+	/* Null-terminate the resulting string */
+	str[len_1 + len_2] = '\0';
 
 	return (str);
 }
