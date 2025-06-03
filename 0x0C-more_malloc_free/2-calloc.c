5b98c1124598f729d2f6dbc77437b84a889a8038
eddcode123 <edwinmutugi29@gmail.com>
2025-06-03 13:41:20 +0300

    C progrsm that mimics calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,26 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
-* _calloc - allocate memory using malloc and initialize it to zero
-*@nmemb: number of elements
-*@size: size of the memory block to be allocated
-*
-*Return: poiner to the address of the memory block
+ * calloc - Fucumction that mimics calloc
+ * @nmemb: number of elements of array
+ * @size: size of each element in bytes
+ *
+ * Return: pointer to array or NULL
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *block;
-	unsigned int i;
+	void *array;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
-	block = malloc(nmemb * size);
-	if (block != NULL)
-	{
-		for (i = 0; i < (nmemb * size); i++)
-			block[i] = 0;
-		return (block);
-	}
-	else
+
+	array = malloc(nmemb * sizeof(size));
+
+	if (!array)
 		return (NULL);
+
+
+	return (array);
 }
