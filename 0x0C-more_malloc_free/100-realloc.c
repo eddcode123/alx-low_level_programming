71cac96dd5eb05c00463dfe55c6f769665fcaa50
eddcode123 <edwinmutugi29@gmail.com>
2025-06-09 12:48:38 +0300

    C program that mimics the functionality of the realloc function

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,44 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
-
 /**
-* _realloc - reallocates a memory block
-* @ptr: pointer to the memory previously allocated with a call to malloc
-* @old_size: size of ptr
-* @new_size: size of the new memory to be allocated
-*
-* Return: pointer to the address of the new memory block
+ * _realloc - Fuction that mimics the functionality of realloc
+ * @ptr: pointer to previous allocated memory
+ * @old_size: size of the old memory
+ * @new_size: size of the new memory
+ * Return: pointer to reallocated memory or NULL
 */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *temp_block;
-	unsigned int i;
+	void *ptr1;
+	unsigned int size, i;
 
-	if (ptr == NULL)
+	if (new_size == old_size)
 	{
-		temp_block = malloc(new_size);
-		return (temp_block);
-	}
-	else if (new_size == old_size)
 		return (ptr);
+	}
 
-	else if (new_size == 0 && ptr != NULL)
+	if (!ptr)
+	{
+		return (malloc(new_size));
+	}
+
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	else
+	ptr1 = malloc(new_size);
+
+	if (!ptr1)
 	{
-		temp_block = malloc(new_size);
-		if (temp_block != NULL)
-		{
-			for (i = 0; i < min(old_size, new_size); i++)
-				*((char *)temp_block + i) = *((char *) ptr + i);
-			free(ptr);
-			return (temp_block);
-		}
-		else
-			return (NULL);
+		return (NULL);
 	}
+
+	size = (old_size < new_size) ? old_size : new_size;
+
+	for (i = 0; i < size; i++)
+	{
+		((char *)ptr)[i] = ((char *)ptr1)[i];
+	}
+
+	free(ptr);
+	return (ptr1);
 }
