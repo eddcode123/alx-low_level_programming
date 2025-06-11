fe3c6cb1200b40e809b5c6bafc65d15e2f0d8f10
eddcode123 <edwinmutugi29@gmail.com>
2025-06-11 11:41:02 +0300

    C header file that defines a structer that represents a dog object

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,31 +1,16 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-
 /**
- * doggy - Typedef for struct dog
- */
-typedef struct dog dog_t;
-
-/**
- * struct dog - struct containing some dog info
- * @name: name of dog
- * @age: age of dog
- * @owner: name of dog owner
- *
- * description: basically this struct will be used to
- * to collect data on dog name age and dog owner name
- *
- */
+ * struct dog - describes a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of owner
+ * Description: This structure represent a dog object
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
 #endif
