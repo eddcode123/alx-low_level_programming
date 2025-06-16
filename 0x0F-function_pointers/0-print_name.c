004b04361cd5bb55f6ec5819e1ade452c20e07a1
eddcode123 <edwinmutugi29@gmail.com>
2025-06-16 12:22:46 +0300

    C program that makes a callback to a function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,12 @@
-#include "function_pointers.h"
 #include <stdio.h>
 
 /**
- * print_name - prints a name.
- * @name: input name.
- * @f: function pointer
- *
- * Return: nothing.
- */
+ * print_name - Function that prints a name
+ * @name: pointer to name
+ * @f: fuction pointer
+ * Return: nothing
+*/
 void print_name(char *name, void (*f)(char *))
 {
-	/* check if name and f are null */
-	if (name != NULL && f != NULL)
-		/* if true not true */
-		f(name);
+	f(name);
 }
