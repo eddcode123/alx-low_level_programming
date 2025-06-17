6109f7e7041006c5066e33fea8184499d8f79854
eddcode123 <edwinmutugi29@gmail.com>
2025-06-17 13:08:54 +0300

    pushing main function

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,45 +1,38 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - check code
- * @argc: number of arguments passed
- * @argv: array of strings
- * Return: alway 0
- */
-
+ * main - function that performs arithimetic
+ * operation on commandline arguments passed
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always 0
+*/
 int main(int argc, char **argv)
 {
+	int (*result)(int, int);
 	int a, b;
-	int (*opp)(int, int);
 
-
-	/* check for argument count */
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	/* assign address of function pointer */
-	opp = get_op_func(argv[2]);
-
-	/* check for null */
-	if (opp == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	/* convert string to integer */
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	/* print result */
-	printf("%d\n", opp(a, b));
+	result = get_op_func(argv[2]);
+
+	if (result ==  NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", result(a, b));
+
 	return (0);
 }
