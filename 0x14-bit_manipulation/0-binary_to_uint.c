e988765dbddad2d2cc26efd98c991534d86d0bdd
eddcode123 <edwinmutugi29@gmail.com>
2025-07-16 11:56:54 +0300

    C program that converts a binary number to unsigned int value

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,21 +10,28 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int value = 0;
-	unsigned int i;
+	unsigned int i, len;
 
 	if (b == NULL)
 	{
 		return (0);
 	}
 
-	for (i = 0; b[i] != '\0'; i++)
+	for (len = 0; b[len] != '\0'; len++)
 	{
-		if (b[i] != '1' && b[i] != '0')
+
+	}
+
+	for (i = 0; len > 0; i++, len--)
+	{
+		if (b[len - 1] != '1' && b[len - 1] != '0')
 		{
 			return (0);
 		}
-		value = value * 2 + (b[i] - '0');
+		if (b[len - 1] == '1')
+		{
+			value += (1 << i);
+		}
 	}
-
 	return (value);
 }
